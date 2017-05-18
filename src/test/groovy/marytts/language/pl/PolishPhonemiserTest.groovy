b3315b143934a6ed0da15368aac37ffdda4615e6
Ingmar Steiner <steiner@coli.uni-saarland.de>
2017-05-18 11:44:31 +0200

    add parameterized phonemiser tests, using groovy

---
+++ src/test/groovy/marytts/language/pl/PolishPhonemiserTest.groovy
@@ -0,0 +1,72 @@
+package marytts.language.pl
+
+import groovy.xml.StreamingMarkupBuilder
+import groovy.xml.XmlUtil
+import marytts.datatypes.MaryData
+import marytts.util.dom.DomUtils
+import org.custommonkey.xmlunit.*
+import org.testng.Assert
+import org.testng.annotations.*
+
+/**
+ * @author ingmar
+ */
+class PolishPhonemiserTest {
+
+    def phonemiser
+
+    @BeforeSuite
+    void setUp() {
+        phonemiser = new PolishPhonemiser()
+    }
+
+    /**
+     * Add any number of lemmas with their corresponding expected transcription to the map below.
+     * @return data provider 2-D array for parameterized testing
+     */
+    @DataProvider
+    public Object[][] data() {
+        return this.getClass().getResourceAsStream('phonemiser_testdata.txt')?.readLines()?.collect { line ->
+            def (lemma, transcription, pos, method) = line.split('\t')
+            [lemma, transcription, pos, method]
+        }
+    }
+
+    @Test(dataProvider = 'data')
+    void testProcess(String lemma, String expectedTranscription, String expectedPos, String expectedMethod) {
+        // create XML
+        def xmlStr = new StreamingMarkupBuilder().bind {
+            mkp.declareNamespace 'xsi': 'http://www.w3.org/2001/XMLSchema-instance', '': 'http://mary.dfki.de/2002/MaryXML'
+            maryxml('xml:lang': 'pl', version: 0.5) {
+                p {
+                    s {
+                        phrase {
+                            t pos: expectedPos, lemma
+                        }
+                    }
+                }
+            }
+        }.toString()
+
+        // wrap XML into input MaryData
+        def input = new MaryData(phonemiser.inputType, new Locale('pl'))
+        input.document = DomUtils.parseDocument(xmlStr)
+
+        // expected XML string
+        def xml = new XmlSlurper(false, false).parseText(xmlStr)
+        def token = xml.p.s.phrase.t[0]
+        token.@g2p_method = expectedMethod
+        token.@ph = expectedTranscription
+        def expected = XmlUtil.serialize(xml)
+
+        // actual XML string after processing
+        def output = phonemiser.process(input)
+        def actual = DomUtils.serializeToString(output.document)
+
+        // compare XML
+        XMLUnit.ignoreWhitespace = true
+        def diff = XMLUnit.compareXML(expected, actual)
+        def detailedDiff = new DetailedDiff(diff)
+        Assert.assertTrue(detailedDiff.identical(), detailedDiff.toString())
+    }
+}
